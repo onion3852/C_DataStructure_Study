44aed0d34e567bf08ae0d76f45c041954487d36c
Dongwook Kim <ddong3852@gmail.com>
2023-02-19 18:55:12 +0900

    update_version3

--- PhoneBook_example/phonebook_v2.c
+++ PhoneBook_example/phonebook_v2.c
@@ -17,6 +17,7 @@ void status();
 void delete();
 void load();
 void save();
+int  search();
 
 int main(){
     char command [BUFFER_SIZE];
@@ -27,19 +28,19 @@ int main(){
         // scanf()가 공백문자 입력받기 전 까지를 저장함을 이용
 
         // 두 문자열을 비교하여 같다면 0을 반환하는 strcmp를 이용
-        if(!strcmp(command, "read"))
+        if(strcmp(command, "read") == 0)
             load();
-        else if(!strcmp(command, "add"))
+        else if(strcmp(command, "add") == 0)
             add();
-        else if(!strcmp(command, "find"))
+        else if(strcmp(command, "find") == 0)
             find();
-        else if(!strcmp(command, "status"))
+        else if(strcmp(command, "status") == 0)
             status();
-        else if(!strcmp(command, "delete"))
+        else if(strcmp(command, "delete") == 0)
             delete();
-        else if(!strcmp(command, "save"))
+        else if(strcmp(command, "save") == 0)
             save();
-        else if(!strcmp(command, "exit"))
+        else if(strcmp(command, "exit") == 0)
             break;          
     }
 
@@ -52,15 +53,37 @@ int main(){
 void add(){
     char str1 [BUFFER_SIZE];
     char str2 [BUFFER_SIZE];
+    int i, p;
 
     scanf("%s", str1);
     scanf("%s", str2);
+    // 알파벳 순서대로 정렬하기 위한 part
+    i = search(str1);
+    if(i >= 0){
+        printf("Person named '%s' already exists. Try other name.\n", str1);
+
+        return;
+    }
+
+    // 새로운 사람일 경우
+    for(i = 0; i < n; i++){
+        if(strcmp(str1, names[i]) < 0){
+            for(p = n - 1; p > i - 1; p--){
+                names  [p + 1] = names[p];
+                numbers[p + 1] = numbers[p];
+            }
+            names[i]   = strdup(str1);
+            numbers[i] = strdup(str2);
+            n++;
+            printf("%s was added successfully.\n", str1);
+
+            return;
+        }
+    }
+    // 만약 가장 마지막 순서의 이름일 경우
     names[n]   = strdup(str1);
     numbers[n] = strdup(str2);
-    // strdup()는 내부적으로 malloc()을 통해 메모리를 할당받고 
-    // 거기에 문자열을 복사한다. 그리고 그 공간의 시작점을 return한다.(포인터)
     n++;
-
     printf("%s was added successfully.\n", str1);
 }
 
@@ -69,11 +92,9 @@ void find(){
     int i;
 
     scanf("%s", str1);
-    for(i = 0; i < n; i++){
-        if(strcmp(str1, names[i]) == 0){
-            printf("%s\n", numbers[i]);
-            return;
-        }
+    if((i = search(str1)) >= 0){
+        printf("%s's number is %s.\n",names[i], numbers[i]);
+        return;
     }
     printf("Person named '%s' isn't exists.\n", str1);
 }
@@ -84,7 +105,7 @@ void status(){
     for(i = 0; i < n; i++)
         printf("%s : %s\n", names[i], numbers[i]);
 
-    printf("Total %d person\n", n);
+    printf("Total %d persons.\n", n);
 }
 
 void delete(){
@@ -92,22 +113,30 @@ void delete(){
     int i;
 
     scanf("%s", str1);
-    // 맨 마지막 저장 데이터를 없앨 데이터 위치로 옮긴다.
-    // 실제 삭제 과정이 필요한 것이 아님
-    for(i = 0; i < n; i ++){
-        if(strcmp(str1, names[i]) == 0){
-            names[i]   = names[n - 1];
-            numbers[i] = numbers[n - 1];
+    if((i = search(str1)) >= 0){
+        for(; i < n - 1; i++){
+            names[i]   = names[i + 1];
+            numbers[i] = numbers[i + 1];
             n--;
             printf("'%s' was deleted successfully.\n", str1);
             
             return;
         }
+        // if i == n
+        names[i] = '\0';
+        numbers[i] = '\0';
+        n--;
+        printf("'%s' was deleted successfully.\n", str1);
+            
+        return;
     }
     printf("Person named '%s' isn't exists.\n", str1);
 }
 
 void load(){
+    // 파일을 불러오기 위해 호출되는 함수
+    // read 명령의 format은
+    // read <file_name>
     char str1 [BUFFER_SIZE];
     char buf1 [BUFFER_SIZE];
     char buf2 [BUFFER_SIZE];
@@ -134,7 +163,7 @@ void load(){
 
 void save(){
     // save 명령의 입력 format은 
-    // save as "file_name"
+    // save as <file_name>
     char str1 [BUFFER_SIZE];
     char str2 [BUFFER_SIZE];
     int i;
@@ -151,6 +180,19 @@ void save(){
     for(i = 0; i < n; i++){
         fprintf(fp, "%s ", names[i]);
         fprintf(fp, "%s\n", numbers[i]);
+        printf("%s was saved successfully.\n", str2);
     }
     fclose(fp);
+}
+
+int search(char *name){
+    // function 내부에서 활용할 이름 검색 기능
+    int index;
+
+    for(index = 0; index < n; index++){
+        if(strcmp(name, names[index]) == 0)
+            return index;
+    }
+    // if not exists, return -1
+    return -1;
 }
