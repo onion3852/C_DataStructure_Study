9b309e2959c91a58dd3eb522ab0606b437e46c2b
Dongwook Kim <ddong3852@gmail.com>
2023-03-03 22:52:44 +0900

    update

--- Linked_List/basic_5.c
+++ Linked_List/basic_5.c
@@ -1,7 +1,7 @@
 // 연결 리스트를 수정(추가, 삭제 등)하는 다양한 함수들 만들기
-// - 리스트의 n번쨰 주소를 반환
-// - 리스트의 n번째 위치에 노드 삽입
-// - 리스트의 n번째 노드를 삭제하고 그 노드의 데이터를 반환
+// - 리스트의 n번 index 노드 주소를 반환
+// - 리스트의 n번 index 위치에 노드 삽입
+// - 리스트의 n번 index 노드를 삭제하고 그 노드의 데이터를 반환
 // - 입력한 string을 가진 노드를 찾고 삭제하고 그 데이터 반환
 // - 데이터가 순서대로 정렬되어 있을 때, 새 데이터(노드)를 순서에 맞게 삽입
 
@@ -17,11 +17,13 @@ struct node {
 typedef struct node NODE;
 NODE *head = NULL;
 
-NODE *get_addr();
-void insert_index();
+NODE *get_node(int index);
+int  insert_index(int index, char *item);
 char *delete_index();
 char delete_word();
 void insert_ordered();
+void add_first(char *item);
+int  add_after(NODE *ptr, char *item);
 
 
 int main(){
@@ -40,7 +42,18 @@ int main(){
     q->next = NULL;
     p->next = q;
 
-    // 각 함수 순서대로, 1~2개씩 모두 이용하는 코드 작성
+    // 함수들 검증
+    for(int i = 0; i < 3; i++)
+        printf("address of the index %d node is %p\n",i, get_node(i));
+    printf("\n");
+
+    insert_index(0, "Ant");
+    insert_index(3, "FPGA");
+    for(int i = 0; i < 5; i++)
+        printf("%s\n", get_node(i)->data);
+    printf("\n");
+
+
 
 
     return 0;
@@ -51,19 +64,51 @@ int main(){
 // function declaration
 /////////////////////////////////////////////////////////////
 
-// 리스트의 n번쨰 주소를 반환
-NODE *get_addr(){
+// 리스트의 어떤 index(0부터)에 해당하는 노드 주소를 반환
+// 포인터를 반환하므로 주소출력시 이를 주의
+NODE *get_node(int index){
+    if(index < 0)
+        return NULL;
+
+    NODE *ptr = head;
+    for(int i = 0; (i < index) && (ptr != NULL); i++)
+        ptr = ptr->next;
     
+    return ptr;
 }
 
-// 리스트의 n번째 위치에 노드 삽입
-void insert_index(){
+// 리스트의 index n번 위치에 노드 삽입
+// 새로운 n번 index가 된다는 의미
+// 성공적으로 완료시 1 반환
+int insert_index(int index, char *item){
+    if(index < 0)
+        return NULL;
     
+    if(index == 0)
+        add_first(item);
+    return 1;
+
+
+    NODE *ptr = get_node(index - 1);
+    if(ptr != NULL){
+        add_after(ptr, item);
+        return 1;
+    }
+
+    return 0;
 }
 
-// 리스트의 n번째 노드를 삭제하고 그 노드의 데이터를 반환
-char *delete_index(){
+// 리스트의 n번 index 노드를 삭제하고 그 노드의 데이터를 반환
+char *delete_index(int index){
+    if(index <0 )
+        return NULL;
     
+    if(index == 0)
+        head = head->next;
+    return
+    
+
+    ptr->next = 
 }
 
 // 입력한 string을 가진 노드를 찾고 삭제하고 그 데이터 반환
@@ -76,3 +121,29 @@ void insert_ordered(){
     
 }
 
+
+/////////////////////////
+// 함수 내에서 쓸 함수들 //
+/////////////////////////
+
+// 리스트의 맨 앞에 삽입시키는 함수
+void add_first(char *item){
+    NODE *tmp = (NODE *)malloc(sizeof(NODE));
+    tmp->data = item;
+    tmp->next = head;
+    head      = tmp;
+}
+
+// 어떤 노드 다음 위치에 새로운 노드 삽입시키는 함수
+// 성공시 1, 실패시 0 반환
+int add_after(NODE *ptr, char *item){
+    if(ptr == NULL)
+        return 0;
+
+    NODE *tmp = (NODE *)malloc(sizeof(NODE));
+    tmp->data = item;
+    tmp->next = ptr->next;
+    ptr->next = tmp;
+
+    return 1;
+}
