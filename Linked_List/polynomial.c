d62384f5cdd631f7f70603ad81ad2ea1612d563a
Dongwook Kim <ddong3852@gmail.com>
2023-03-05 19:13:33 +0900

    update

--- Linked_List/polynomial.c
+++ Linked_List/polynomial.c
@@ -5,7 +5,8 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAX_POLYS 25
+#define MAX_POLYS  25
+#define BUFFER_LEN 100
 
 // 다항식에서 각 항에 해당하는 구조체를 정의함
 typedef struct term {
@@ -25,9 +26,19 @@ POLY *polys[MAX_POLYS];  // 다항식들로 구성한 배열(배열의 이름이
 int n = 0;  // 저장된 다항식들의 개수
 
 
+void process_command();
+int  read_line(FILE *fp, char *str, int limit);
+void handle_print();
+void handle_calc();
+void handle_define();
 TERM *create_term();
 POLY *create_poly(char *name);
 void add_term(POLY *poly, int c, int e);
+int  calculate(POLY *poly, int x);
+int  cal(TERM *ptr, int x);
+void print_poly(POLY *poly);
+void print_term(POLY *poly, TERM *ptr);
+
 
 
 int main(){
@@ -39,6 +50,102 @@ int main(){
 ///////////////////////////////////////////////////////////////////////
 // FUNCTION
 //////////////////////////////////////////////////////////////////////
+
+// 프롬프트 명령을 처리하는 함수
+void process_command(){
+    char command_line[BUFFER_LEN];
+    char command_copy[BUFFER_LEN];
+    char *command;
+    char *arg1;
+    char *arg2;
+
+    while(1){
+    printf("$ ");
+
+    // 아무것도 입력되지 않은 경우
+    if(read_line(stdin, command_line, BUFFER_LEN) <= 0)
+        continue;
+    
+    strcpy(command_copy, command_line);  // copy the command for further use
+    command = strtok(command_copy, " ");
+    
+    if(strcmp(command, "print") == 0){
+        arg1 = strtok(NULL, " ");
+        if(arg1 == NULL){
+            printf("Error : Polynomial name is required\n");
+            continue;
+        }
+        handle_print(arg1);
+    }  // "print" 명령
+
+    else if(strcmp(command, "calc")){
+        arg1 = strtok(NULL, " ");   // poly name
+        if(arg1 == NULL){
+            printf("Error : Polynomial name is required\n");
+            continue;
+        }
+
+        arg2 = strtok(NULL, " ");   // value for x
+        if(arg2 == NULL){
+            printf("Error : value for 'x' is required\n");
+            continue;
+        }
+
+
+    }  // "calc" 명령
+
+    else if(strcmp(command, "exit") == 0){
+        printf("Exit...\n");
+        break;
+    }  // "exit" 명령
+}
+
+// 프롬프트에 작성한 명령을 읽어들이는 함수
+int read_line(FILE *fp, char *str, int limit){
+    int ch;  // getchar()의 리턴은 int type이어야 한다는 점 주의
+    int i = 0;
+
+    // 두 번째 조건식은 파일을 읽을 때 필요
+    while( ((ch = fgetc(fp)) != '\n') && (ch != EOF) ){
+        if(i < limit - 1)
+            str[i] = ch;
+
+        i++;
+    }
+    // 만약 i 가 'limit' 까지 증가
+    // 또는 i 가 0일 때 \n 만날 경우
+    str[i] = '\0';
+
+    return i;
+}
+
+// 다항식 출력 함수
+void handle_print(char *str){
+    int i = 0;
+
+    while((strcmp(str, polys[i]->name) != 0)){
+        i++;
+    }
+    // match되는 이름의 poly가 없는 경우
+    if(i == n){
+        printf("Error : Polynomial '%s' isn't exists\n", str);
+
+        return;
+    }
+    
+    printf("%s = ");
+    print_poly(polys[i]);
+}
+
+// 다항식 계산 함수
+void handle_calc(){
+
+}
+
+// 다항식 정의 함수
+void handle_define(){
+
+}
 
 // 새 term 구조체 공간을 동적 할당받고
 // 멤버들을 초기화 시키는 함수
@@ -114,3 +221,68 @@ void add_term(POLY *poly, int c, int e){
     
     return;
 }
+
+// 다항식을 계산하는 함수
+// 다항식과 미지수를 선택
+int calculate(POLY *poly, int x){
+    int result = 0;
+    TERM *p = poly->first;
+
+    while(p != NULL){
+        result += cal(p, x);
+        p = p->next;
+    }
+
+    return result;
+}
+
+// 특정 항에 대한 계산을 하는 함수
+int cal(TERM *ptr, int x){
+    int value = ptr->coef;
+
+    for(int i = 0; i < ptr->expo; i++)
+        value *= x;
+    
+    return value;
+}
+
+// 다항식을 터미널에 출력하는 함수
+void print_poly(POLY *poly){
+    printf("%s = ", poly->name);
+    TERM *p = poly->first;
+
+    while(p != NULL){
+        print_term(poly, p);
+        p = p->next;
+    }
+}
+
+// 각 항을 print하는 함수
+// +/-, 1제곱, 0제곱 등의 예외 판단 기능을 포함
+void print_term(POLY *poly, TERM *ptr){
+    char sign;
+
+    // + 부호 출력여부 판단
+    if((ptr != poly->first) && (ptr->coef > 0)){
+        printf("+");
+    }
+
+    printf("%d", ptr->coef);
+
+    // 차수가 1인 경우
+    if(ptr->expo == 1){
+        printf("x ");
+        
+        return;
+    }
+    // 차수가 0인경우
+    else if(ptr->expo == 0){
+        return;
+    }
+    // 차수가 2이상인 경우
+    else
+        printf("x^%d ", ptr->expo);
+
+    return;
+}
+
